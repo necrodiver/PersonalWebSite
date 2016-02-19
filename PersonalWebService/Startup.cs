b62112416d5c86393ae2bac47ada0c5dc50acc50
neclodiver <neclodiver@live.com>
2016-02-19 17:17:49 +0800

    添加内容

---
+++ PersonalWebService/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PersonalWebService.Startup))]
+
+namespace PersonalWebService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
