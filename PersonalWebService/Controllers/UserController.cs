9c92a769b0a81a166e57c81049b65f2cda17d554
neclodiver <neclodiver@live.com>
2016-02-21 22:27:12 +0800

    添加内容

--- PersonalWebService/Controllers/UserController.cs
+++ PersonalWebService/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PersonalWebService.ControllerBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,32 +8,32 @@ using System.Web.Http;
 
 namespace PersonalWebService.Controllers
 {
-    [Authorize]
-    public class ValuesController : ApiController
+    public class UserController : BaseController
     {
-        // GET api/values
+        // GET: api/User
+        [Route("User")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET: api/User/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/User
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/User/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/User/5
         public void Delete(int id)
         {
         }
