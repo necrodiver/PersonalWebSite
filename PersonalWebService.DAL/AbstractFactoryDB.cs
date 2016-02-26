08ee55842d590db10e5c90449908d979732f7db1
neclodiver <neclodiver@live.com>
2016-02-26 17:02:55 +0800

    补充添加内容

---
+++ PersonalWebService.DAL/AbstractFactoryDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebService.DAL
+{
+    public abstract class AbstractFactoryDB
+    {
+        public abstract T Add<T>();
+        public abstract T Delete<T>();
+        public abstract T Get<T>();
+        public abstract T Edit<T>();
+    }
+}
---
+++ PersonalWebService.DAL/OperateDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebService.DAL
+{
+    public class OperateDB : AbstractFactoryDB
+    {
+        public override T Add<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Delete<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Edit<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
