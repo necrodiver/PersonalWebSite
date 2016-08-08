260d2abfa50e84ae79905d707eec009c1aab15c8
neclodiver <neclodiver@live.com>
2016-08-08 11:10:40 +0800

    整理enum

--- PersonalWebService.Model/Enums.cs
+++ PersonalWebService.Model/Enums.cs
@@ -55,4 +55,34 @@ namespace PersonalWebService.Model
         /// </summary>
         Get
     }
+    /// <summary>
+    /// 错误等级
+    /// </summary>
+    public enum LogLevels
+    {
+        /// <summary>
+        /// 1/6.致命性错误
+        /// </summary>
+        Fatal,
+        /// <summary>
+        /// 2/6.标准错误
+        /// </summary>
+        Error,
+        /// <summary>
+        /// 3/6.警告
+        /// </summary>
+        Warn,
+        /// <summary>
+        /// 4/6.信息
+        /// </summary>
+        Info,
+        /// <summary>
+        /// 5/6.调试消息
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// 6/6.设置信息
+        /// </summary>
+        Trace
+    }
 }
