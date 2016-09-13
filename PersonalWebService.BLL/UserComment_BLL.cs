775d5a504d49cded3bd7b14311327824c5191d9f
neclodiver <neclodiver@live.com>
2016-09-13 15:48:27 +0800

    添加修改评论内容，和部分内容的修改

---
+++ PersonalWebService.BLL/UserComment_BLL.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalWebService.Model;
+using PersonalWebService.DAL;
+using PersonalWebService.Helper;
+
+namespace PersonalWebService.BLL
+{
+    public class UserComment_BLL
+    {
+        private static readonly string sqlSelectTemplate = "SELECT {0} FROM [dbo].[UserComment] WHERE {1}";
+        private static readonly string sqlDeleteTemplate = "DELETE [dbo].[UserComment] where {0}";
+        private static readonly string sqlUpdateTemplate = "UPDATE [dbo].[UserComment] SET {0} WHERE {1}";
+        private static IDAL.IDAL_PersonalBase dal = new Operate_DAL();
+        public ReturnStatus_Model AddComment(UserComment_Model userComment)
+        {
+            ReturnStatus_Model rsModel = new ReturnStatus_Model();
+            rsModel.title = "添加评论";
+            rsModel.isRight = false;
+            try
+            {
+                string sqltemp1 = @"SELECT Count(*) FROM Article,Picture WHERE Article.ArticleId=@WorkId or Picture.PictureId=@WorkId ";
+                if (dal.GetDataCount(sqltemp1, new DataField { Name = "@WorkId", Value = userComment.WorkId }) <= 0)
+                {
+                    rsModel.message = "您所要评论的文章或图片找不到了，请刷新后重试~~";
+                    return rsModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogRecord_Helper.RecordLog(LogLevels.Fatal, ex.ToString());
+                rsModel.message = "评论出错，请稍后重试";
+                return rsModel;
+            }
+            UserInfo userInfo = SessionState.GetSession<UserInfo>("UserInfo");
+            if (userInfo == null)
+            {
+                rsModel.isRight = false;
+                rsModel.message = "您已退出登录或当前未登录，请先登录后再进行评论";
+                return rsModel;
+            }
+            UserComment userCommentModel = new UserComment();
+            userCommentModel.ReText = userComment.ReText;
+            userCommentModel.WorkId = userComment.WorkId;
+            userCommentModel.UserId = userInfo.UserId;
+            userCommentModel.CommentTime = DateTime.Now;
+            userCommentModel.CommentParentId = userComment.CommentParentId;
+            try
+            {
+                if (dal.OpeData<UserComment>(userCommentModel, OperatingModel.Add))
+                {
+                    rsModel.isRight = true;
+                    rsModel.message = "评论成功！";
+                    return rsModel;
+                }
+                else
+                {
+                    rsModel.isRight = false;
+                    rsModel.message = "评论失败，请稍后重试";
+                    return rsModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogRecord_Helper.RecordLog(LogLevels.Fatal, ex.ToString());
+                rsModel.message = "评论出错，请稍后重试";
+                return rsModel;
+            }
+        }
+
+        public List<UserComment_Model> GetList(UserCommentCondition_Model condition)
+        {
+            //搁着下次写
+        }
+
+        public ReturnStatus_Model DeleteSingle(string userCommendId)
+        {
+            ReturnStatus_Model rsModel = new ReturnStatus_Model();
+            rsModel.isRight = false;
+            rsModel.title = "删除评论";
+            UserInfo userInfo = SessionState.GetSession<UserInfo>("UserInfo");
+            string sqlSelect = @"SELECT COUNT(*) FROM [dbo].[UserComment]
+                            WHERE CommentId=@CommentId and UserId=@UserId";
+            List<DataField> paramSelect = new List<DataField>();
+            paramSelect.Add(new DataField { Name = "@CommentId", Value = userCommendId });
+            paramSelect.Add(new DataField { Name = "@UserId", Value = userInfo.UserId });
+            try
+            {
+                if (dal.GetDataCount(sqlSelect, paramSelect) <= 0)
+                {
+                    rsModel.isRight = false;
+                    rsModel.message = "不存在此条评论，无法删除！";
+                    return rsModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogRecord_Helper.RecordLog(LogLevels.Fatal, ex.ToString());
+                rsModel.isRight = false;
+                rsModel.message = "评论删除出错，请稍后重试";
+                return rsModel;
+            }
+            string sql = string.Format(sqlDeleteTemplate, "WHERE CommentId=@CommentId and UserId=@UserId");
+            try
+            {
+                if (dal.OpeData(sql, paramSelect))
+                {
+                    rsModel.isRight = true;
+                    rsModel.message = "删除评论成功！";
+                    return rsModel;
+                }
+                rsModel.isRight = false;
+                rsModel.message = "删除失败，请稍后重试！";
+                return rsModel;
+            }
+            catch (Exception ex)
+            {
+                LogRecord_Helper.RecordLog(LogLevels.Fatal, ex.ToString());
+                rsModel.isRight = false;
+                rsModel.message = "评论删除出错，请稍后重试或联系管理员查看！";
+                return rsModel;
+            }
+
+        }
+    }
+}
---
+++ PersonalWebService/Controllers/CommentController.cs
@@ -0,0 +1,46 @@
+﻿using PersonalWebService.BLL;
+using PersonalWebService.Helper;
+using PersonalWebService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PersonalWebService.Controllers
+{
+    [RoutePrefix("api/Comment")]
+    [ModelValidationFilter]
+    [BasicAuthentication]
+    public class CommentController : ApiController
+    {
+        private static UserComment_BLL userCommentBll = new UserComment_BLL();
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ReturnStatus_Model> Add([FromBody]UserComment_Model userComment)
+        {
+            return await Task.Run(() =>
+            {
+                return userCommentBll.AddComment(userComment);
+            });
+        }
+        [HttpPost]
+        [Route("Delete")]
+        //这个方法先留着，不公开，没必要公开给用户自己删除自己的评论 
+        public async Task<ReturnStatus_Model>DeleteSingle(string userCommendId)
+        {
+            return await Task.Run(()=> {
+                return userCommentBll.DeleteSingle(userCommendId);
+            });
+        }
+
+        public async Task<List<UserComment_Model>>GetList([FromBody]UserCommentCondition_Model condition)
+        {
+            return await Task.Run(()=> {
+                return userCommentBll.GetList(condition);
+            });
+        }
+    }
+}
