652dd28cd7649a891ec1bf58c284c6d7a00385b0
neclodiver <neclodiver@live.com>
2016-09-27 09:30:46 +0800

    测试及添加，以后再去

---
+++ PersonalWebClient/ScriptsAsM/Form.js
@@ -0,0 +1,24 @@
+﻿var userInfoModule = angular.module('UserInfoModule', []);
+userInfoModule.controller('MyCtrl', ['$scope', function ($scope) {
+    $scope.userInfo = {
+        email: '12445123@163.com',
+        password: '162413151',
+        autoLogin: true
+    };
+    $scope.getFormData = function () {
+        console.log($scope.userInfo);
+    }
+    $scope.setFormData = function () {
+        $scope.userInfo = {
+            email: '34673@qq.com',
+            password: 'ut3436', autoLogin: false
+        };
+    }
+    $scope.resetFormData = function () {
+        $scope.userInfo = {
+            email: '12445123@163.com',
+            password: '162413151',
+            autoLogin: true
+        };
+    }
+}]);
---
+++ PersonalWebClient/ScriptsAsM/app.js
@@ -0,0 +1,11 @@
+﻿
+var helloModule = angular.module('app', []);
+helloModule.config(function ($routeProvider) {
+    $routeProvider.when('/hello', {
+        templateUrl: 'template/hello.html',
+        controller: 'HelloCtrl'
+    }).when('/list', {
+        templateUrl: 'template/bookList.html',
+        controller: 'BookListCtrl'
+    }).otherwise({ redirectTo: '/hello' });
+});
