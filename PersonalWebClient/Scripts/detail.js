c31e1c81cee17187a61dc7d83ce96ad251460909
neclodiver <neclodiver@live.com>
2016-12-02 16:50:05 +0800

    修改: 1. 去掉了bootstrap.css的popover 的class属性width值 2.完成了Detail.html的弹出框

---
+++ PersonalWebClient/Scripts/detail.js
@@ -0,0 +1,25 @@
+﻿$('#userIntro').each(function () {
+    var element = $(this);
+    var txt = '用户信息';
+    element.popover({
+        trigger: 'manual',
+        placement: 'right', //top, bottom, left or right
+        title: txt,
+        html: 'true',
+        content: $('#userIntroTemplate').html(),
+    }).on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+    $('#userIntro').popover('hide');
+});
