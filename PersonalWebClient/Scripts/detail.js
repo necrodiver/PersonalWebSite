bac30c18b50b4a167f6644ccf8d9614157ca064c
neclodiver <neclodiver@live.com>
2016-12-08 16:18:21 +0800

    完成Detail.html页面布局

--- PersonalWebClient/Scripts/detail.js
+++ PersonalWebClient/Scripts/detail.js
@@ -24,6 +24,32 @@
     $('#userIntro').popover('hide');
 });
 
+$('.anchor_PlayTour').each(function () {
+    var element = $(this);
+    var txt = '谢谢大佬打赏O(∩_∩)O~~';
+    element.popover({
+        trigger: 'manual',
+        placement: 'left', //top, bottom, left or right
+        title: txt,
+        html: 'true',
+        content: $('#QRcodeTemplate').html(),
+    }).on("mousedown", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(this).siblings(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide")
+            }
+        }, 100);
+    });
+});
+$('.anchor_PlayTour').popover('hide');
+
 $(function () {
     var userCommpntTemplate = $('#userCommentTemplate');
     for (var i = 1; i < 6; i++) {
